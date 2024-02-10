3ac47d0964435d055394740ca9514f36e65dee32
Sashikumar <yshashi30@gmail.com>
2024-02-11 01:38:07 +0800

    fix: the breaking for login and get all users

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,66 +5,73 @@ import { createError } from "../utils/error.js";
 import { createSuccess } from "../utils/success.js";
 import jwt from 'jsonwebtoken'
 
-export const register= async (req,res,next)=>{
-    const role= await Role.find({role:'User'});
-    const salt= await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password,salt)
+export const register = async (req, res, next) =>
+{
+    const role = await Role.find({ role: 'User' });
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
     const newUser = new User({
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        userName:req.body.userName,
-        email:req.body.email,
-        password:hashPassword,
-        roles:role
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: hashPassword,
+        roles: role
     });
     await newUser.save();
-    return next(createSuccess(200,"Registration Success"))
+    return next(createSuccess(200, "Registration Success"))
 }
 
-export const registerAdmin= async (req,res,next)=>{
-    const role= await Role.find({});
-    const salt= await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password,salt)
+export const registerAdmin = async (req, res, next) =>
+{
+    const role = await Role.find({});
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
     const newUser = new User({
-        firstName:req.body.firstName,
-        lastName:req.body.lastName,
-        userName:req.body.userName,
-        email:req.body.email,
-        password:hashPassword,
-        isAdmin:true,
-        roles:role
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: hashPassword,
+        isAdmin: true,
+        roles: role
     });
     await newUser.save();
-    return next(createSuccess(200,"Admin Registered successfully!"))
+    return next(createSuccess(200, "Admin Registered successfully!"))
 }
 
-export const login= async (req,res,next)=>{
-    try {
-        const user= await User.findOne({email:req.body.email})
-        .populate("roles","role");
-        const {roles}=user
-        if(!user){
+export const login = async (req, res, next) =>
+{
+    try
+    {
+        const user = await User.findOne({ email: req.body.email })
+            .populate("roles", "role");
+        const { roles } = user
+        if (!user)
+        {
             return res.status(404).send("user not found")
         }
-        const isPasswordCorrect= await bcrypt.compare(req.body.password,user.password)
-        if(!isPasswordCorrect){
+        const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+        if (!isPasswordCorrect)
+        {
             return res.status(400).send("Password is incorrect")
         }
-        const token=jwt.sign(
-            {id:user._id,isAdmin:user.isAdmin,role:roles},
+        const token = jwt.sign(
+            { id: user._id, isAdmin: user.isAdmin, role: roles },
             process.env.JWT_SECRET
         )
-        res.cookie("access", token,{httpOnly:true})
-        .status(200)
-        .json({
-            status:200,
-            message:"login Success",
-            data:user
-        })
+        res.cookie("access", token, { httpOnly: true })
+            .status(200)
+            .json({
+                status: 200,
+                message: "login Success",
+                data: user
+            })
 
         // return next(createSuccess(200,"Login Success"))
 
-    } catch (error) {
+    } catch (error)
+    {
         return res.status(500).send("Internal Server Error")
     }
 }
